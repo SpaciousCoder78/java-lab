c6816bfc4686d37ed3d0aaceed70f15fd73f865e
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-12-22 13:50:20 +0530

    Create 01_Addition_of3_nos.java

---
+++ 01_Addition_of3_nos.java
@@ -0,0 +1,8 @@
+//addition of 3 nos
+public class Main{
+    public static void main(String[] args){
+        int a = 10,b=10,c=3,sum;
+        sum = a+b+c;
+        System.out.println("Addition of abc ="+sum);
+    }
+}
