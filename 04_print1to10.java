5ed59afbb0fbd2721d311fe5b253813a07604d39
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-12-22 14:01:28 +0530

    Create 04_print1to10.java

---
+++ 04_print1to10.java
@@ -0,0 +1,9 @@
+//print 1-10 numbers in java
+public class Main{
+  public static void main(String[] args){
+    int count=10;
+    for(int i =0;i<=count;i++){
+      System.out.println(i);
+    }
+  }
+}
