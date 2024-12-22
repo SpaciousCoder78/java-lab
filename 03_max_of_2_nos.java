5ec6d1f3396c27f813c78969a89498e1d23692e9
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-12-22 13:59:42 +0530

    Create 03_max_of_2_nos.java

---
+++ 03_max_of_2_nos.java
@@ -0,0 +1,10 @@
+//maximum of 2 nos
+import java.lang.Math;
+
+public class Main{
+  public static void main(String[] args){
+    int x = 30, y = 20;
+    int maxi= Math.max(x,y);
+    System.out.println("Maximum of x & y = " + maxi);
+  }
+}
